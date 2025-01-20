0be920886823ae50bd7fd721411645f3e7157f90
Kasia Albuquerque <kassiademaria@gmail.com>
2025-01-20 18:32:27 -0300

    feat(background): add background image as default

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,14 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 
 const App: React.FC = () => {
   return (
     <Router>
+      <Header />
+      <Footer />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,11 +1,5 @@
-import Header from "../../components/Header/Header";
-
 const App: React.FC = () => {
-  return (
-    <>
-      <Header />
-    </>
-  );
+  return <></>;
 };
 
 export default App;
